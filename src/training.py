331942bf7b646f974e2b5cacb44d4f3692425c25
Gintautas <167891054+GintautasLuksas@users.noreply.github.com>
2026-03-03 21:14:13 +0200

    Training for

--- src/training.py
+++ src/training.py
@@ -1,9 +1,7 @@
-user_input = int(input('Enter a number: '))
-user_input_2 = int(input('Enter a number: '))
-user_input_3 = int(input('Enter a number: '))
+numbers = []
 
-numbers = [user_input, user_input_2, user_input_3]
+for i in range(4):
+    num = int(input(f'Please enter number nr {i}:  '))
+    numbers.append(num)
 
-for i in numbers:
-    print(i)
-
+print(numbers)
