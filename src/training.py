adec68d1dc88d7c2c4392359208032e60150a266
Gintautas <167891054+GintautasLuksas@users.noreply.github.com>
2026-03-04 07:45:24 +0200

    Learning further

--- src/training.py
+++ src/training.py
@@ -1,4 +1,18 @@
-colours = ['blue', 'black', 'yellow']
+import random
+food = []
 
-for c in colours:
-    print(f'This is the color: {c}')
+
+for _ in range(5):
+    user_choice = str(input(f'Please write your favourite food nr{_+1}: '))
+    food.append(user_choice)
+
+print(food)
+
+
+numbers = []
+
+for i in range(10):
+    generated = random.randint(1,10)
+    numbers.append(generated)
+
+print(numbers)
