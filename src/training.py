dac54fc6bb5c1840282a74c2caac6a0339792899
Gintautas <167891054+GintautasLuksas@users.noreply.github.com>
2026-03-05 14:54:01 +0200

    Titanic start

--- src/training.py
+++ src/training.py
@@ -1,18 +1,3 @@
-import random
-food = []
+import pandas as pd
 
-
-for _ in range(5):
-    user_choice = str(input(f'Please write your favourite food nr{_+1}: '))
-    food.append(user_choice)
-
-print(food)
-
-
-numbers = []
-
-for i in range(10):
-    generated = random.randint(1,10)
-    numbers.append(generated)
-
-print(numbers)
+df = pd.read_csv('train.csv')
